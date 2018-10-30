7cc828f23d9682b5af75bab42679bde0e8406774
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-30 22:36:24 +0300

    Add files via upload

---
+++ Yazı 2.R
@@ -0,0 +1,52 @@
+sayi_1 = 7L # ilk sayimizi olusturduk.
+
+sayi_2 = 5L # ikinci sayimizi olusturduk.
+
+toplam = sayi_1 + sayi_2 # iki degeri + operatörü ile topladik.
+
+print("Sayilarin toplami : ") # Düzgün çikti için ekrana bu ifadeyi yazdirdik
+
+print(toplam) # sayilarin toplamini ekrana yazdirdik.
+
+sayi_3 = 12L # üçüncü sayimizi olusturduk.
+
+sayi_4 = 2L # dördüncü sayimizi olusturduk.
+
+fark = sayi_3 - sayi_4 # iki degeri - operatörü ile çikardik.
+
+print("Sayilarin farki : ") # Düzgün çikti için ekrana bu ifadeyi yazdirdik
+
+print(fark) # sayilarin farkini ekrana yazdirdik.
+
+sayi_5 = 22L # besinci sayimizi olusturduk.
+
+sayi_6 = 9L # altinci sayimizi olusturduk.
+
+çarpim = sayi_5 * sayi_6 # iki degeri * operatörü ile çarptik.
+
+print("Sayilarin çarpimi : ") # Düzgün çikti için ekrana bu ifadeyi yazdirdik
+
+print(çarpim) # sayilarin çarpimini ekrana yazdirdik.
+
+sayi_7 = 16L # yedinci sayimizi olusturduk.
+
+sayi_8 = 8L # sekizinci sayimizi olusturduk.
+
+bölüm = sayi_7 / sayi_8 # iki degeri / operatörü ile böldük.
+
+print("Sayilarin bölümü : ") # Düzgün çikti için ekrana bu ifadeyi yazdirdik
+
+print(bölüm) # sayilarin bölümünü ekrana yazdirdik.
+
+sayi_9 = 17L # dokuzuncu sayimizi olusturduk.
+
+sayi_10 = 3L # onuncu sayimizi olusturduk.
+
+mod = sayi_9 %% sayi_10 # iki degeri %% operatörü ile böldük.
+
+print("Sayilarin modu : ") # Düzgün çikti için ekrana bu ifadeyi yazdirdik
+
+print(mod) # sayilarin modunu ekrana yazdirdik.
+
+
+
